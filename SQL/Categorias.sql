24ff24946ea0bbfcf7058fa09873a9fc93ab27c7
Tauan Lima <contato@tauanlima.com.br>
2024-07-29 10:18:11 -0300

    Create Categorias
    View  para trazer de forma unificadas as  categorias

---
+++ SQL/Categorias.sql
@@ -0,0 +1,26 @@
+select 	GI.id as CATEG_ID, 
+		GI.id as CATEG_ID_SUPERIOR, 
+		GI.completename as CATEG_NAME, 
+		GI.`level` as CATEG_LEVEL,
+		gi.completename as CATEG_SUPERIOR
+from glpi_itilcategories GI
+where GI.`level` = '1'
+union
+select 	GI.id as CATEG_ID, 
+		GI.itilcategories_id as CATEG_ID_SUPERIOR, 
+		GI.completename as CATEG_NAME, 
+		GI.`level` as CATEG_LEVEL,
+		SUP.completename as CATEG_SUPERIOR
+from 	glpi_itilcategories GI 
+ 		left join glpi_itilcategories SUP on (GI.itilcategories_id = SUP.ID) and (sup.level = '1')
+where GI.`level` = '2'
+union 
+select 	GI.id as CATEG_ID, 
+		GI2.itilcategories_id as CATEG_ID_SUPERIOR, 
+		GI.completename as CATEG_NAME, 
+		GI.`level` as CATEG_LEVEL,
+		SUP.completename as CATEG_SUPERIOR
+from 	glpi_itilcategories GI 
+		LEFT join glpi_itilcategories gi2 on (GI.itilcategories_id = GI2.ID)
+		left join glpi_itilcategories SUP on (gi2.itilcategories_id = sup.id) and (sup.level = '1')
+where GI.`level` = '3';
